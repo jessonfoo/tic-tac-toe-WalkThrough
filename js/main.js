1d9a5aef348ac8cf73c4acff910592564f294e0f
jessonfoo <jessonfoo@gmail.com>
2016-06-28 13:31:04 -0700

    complete

--- js/main.js
+++ js/main.js
@@ -1,31 +1,69 @@
-var board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-var turn = 'x';
-
-// selector
+var board = ['','','','','','','','',''];
+var turn = 'X';
 var squares = document.querySelectorAll('.square');
+// selector
 
+function onLoad(){
+// loop through squares    
+  for (var i = 0; i < 9 ; i++){
+    // add Event Listener to each square
+    squares[i].addEventListener('click',function(e){
+        // get index of square
+       var squareIndex = getSquareIndex(e.target)
+       if(board[squareIndex] == ''){
+         board[squareIndex] = turn;
+         drawBoard();
+         switchTurn();
+       }
+    })
+  };
+  // click Listener Reset
+  document.getElementById('reset').addEventListener('click',function(event){
+    board = ['','','','','','','','',''];
+    drawBoard();
+  });
+}
 // listeners
-for (var i = 0; i < 9 ; i++){
-  squares[i].addEventListener('click',function(e){
-    for (var j=0; j < 9; j++){
-      if(squares[j] === e.target){
-        board[j] = turn;
-        drawBoard();
-        switchTurn();
-      }
+
+
+// return index of target square
+function getSquareIndex(target){
+  for (var j=0;j<9;j++){
+    if(squares[j] == target){
+      return j;
     }
-  })
+  }
 }
 
+// draw array of values to screen
 function drawBoard(){
   for (var i = 0; i < 9; i++){
     squares[i].innerHTML = board[i];
   }
+       checkWin();
 }
+// change turns
 function switchTurn(){
-  if (turn === 'x'){
-    turn ='o';
+  if (turn === 'X'){
+    turn ='O';
   }else {
-    turn ='x';
+    turn ='X';
   }
 }
+function checkWin(){
+  var winCombos = [
+    [0,3,6],
+    [1,4,7],
+    [2,6,8],
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,6],
+    [2,4,6]
+  ];
+  for (var k = 0; k < winCombos.length; k++){
+    var winCombo = winCombos[k];
+    if ((board[winCombo[0]] === board[winCombo[1]]) && (board[winCombo[2]] === board[winCombo[1]]) && (board[winCombo[0]] !== '')) {alert(turn + ' is the winner !');};
+  }
+}
+window.addEventListener('load',onLoad,false);
